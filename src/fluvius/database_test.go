7854c6d198e6c89b941e419b3ef9accba027f2cf
Matthias Luebken <matthias.luebken@gmail.com>
2013-11-26 15:30:30 +0100

    testing merging

--- src/fluvius/database_test.go
+++ src/fluvius/database_test.go
@@ -5,13 +5,16 @@ import (
 	. "github.com/onsi/gomega"
 )
 
-var bm1 = Bookmark{"Google", "http://google.de", "test", "test", "test"}
-var bm2 = Bookmark{"Bing", "http://bing.de", "test", "test", "test"}
+var bm1 = Bookmark{"Google", "http://google.de", "A Comment", "Oli", "a feed"}
+var bm1b = Bookmark{"Google", "http://google.de", "Another Comment", "Matthias", "matthias feed"}
+var bm2 = Bookmark{"Bing", "http://bing.de", "A second comment", "Mattes", "another feed"}
 var ka1 = Karma{"http://google.de", "https://news.ycombinator.com/item?id=1", 1, "test"}
 var ka2 = Karma{"http://bing.de", "http://www.reddit.com/r/technology/comments/1rffrf", 1, "test"}
 
 var _ = Describe("Database", func() {
+
 	Context("Bookmarks", func() {
+
 		It("should be empty", func() {
 			Expect(len(db.Bookmarks(0))).To(Equal(0))
 		})
@@ -33,9 +36,25 @@ var _ = Describe("Database", func() {
 			db.removeEventListener(listener)
 			close(done)
 		})
+		It("a second bookmark should update an existing one", func(done Done) {
+			listener := make(chan interface{})
+			db.addEventListener(listener)
+			db.SaveBookmark <- bm1b
+			Expect(<-listener).To(Equal(bm1b))
+			Expect(len(db.Bookmarks(0))).To(Equal(2))
+			Expect(len(db.Bookmarks(1))).To(Equal(1))
+			Expect(db.Bookmarks(1)[0].Title).To(Equal(bm1.Title))
+			Expect(db.Bookmarks(1)[0].Link).To(Equal(bm1.Link))
+			Expect(db.Bookmarks(1)[0].User).To(Equal(bm1.User + ", " + bm1b.User))
+			Expect(db.Bookmarks(1)[0].Comment).To(Equal("Oli:A Comment, Matthias:Another Comment"))
+			db.removeEventListener(listener)
+			close(done)
+		})
+
 	})
 
 	Context("Karma", func() {
+
 		It("should be empty", func() {
 			Expect(len(db.karmas)).To(Equal(0))
 		})
